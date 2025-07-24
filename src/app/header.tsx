e8f3822bb26da9d9a8a0da2c4b28bd119ab85c51
Emma Lucas <33404272+emlucas@users.noreply.github.com>
2025-07-24 13:17:51 -0500

    Add initial info

---
+++ src/app/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header className={'flex w-full justify-end'}>
+			<span>About</span>
+			<span>Contact</span>
+		</header>
+	)
+}
---
+++ src/app/homepage/headline.tsx
@@ -0,0 +1,9 @@
+export default function Headline() {
+	return (
+		<section className={'flex flex-col items-start justify-center w-full'}>
+			<div>Hi, I'm</div>
+			<div className={'font-bold text-6xl'}>Emma Lucas</div>
+			<div className={'tracking-wide'}>Full-stack web developer</div>
+		</section>
+	)
+}
