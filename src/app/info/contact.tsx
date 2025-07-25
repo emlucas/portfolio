7c70377ed5d84d563dea15be4b3217c5a142eb64
Emma Lucas <33404272+emlucas@users.noreply.github.com>
2025-07-25 16:38:25 -0500

    add social icons

--- src/app/info/contact.tsx
+++ src/app/info/contact.tsx
@@ -1,14 +1,20 @@
 const Contact = () => {
 	return (
 		<section className={'flex flex-col text-xl py-8'} id={'contact'}>
-			<h3 className={'font-medium mb-2'}>Get in touch</h3>
+			<h3 className={'font-medium mb-2'}>Get in touch.</h3>
 			<div>
 				<span>Email: </span>
 				<a className={'underline'} href={'mailto:emmalucas256@gmail.com'}>emmalucas256@gmail.com</a>
 			</div>
-			<div className={'flex gap-4'}>
-				<a href={'https://www.linkedin.com/in/emma-lucas-0241b4155/'} target={'_blank'}>Linkedin</a>
-				<a href={'https://github.com/emlucas'} target={'_blank'}>Github</a>
+			<div className={'flex gap-3 mt-2'}>
+				<a href={'https://www.linkedin.com/in/emma-lucas-0241b4155/'} target={'_blank'}>
+					<img src={'linkedin.svg'} alt={'LinkedIn'} width={25} height={25}
+						  className={'shadow-rose-400 hover:shadow-md'}/>
+				</a>
+				<a href={'https://github.com/emlucas'} target={'_blank'}>
+					<img src={'github.svg'} alt={'Github'} width={25} height={25}
+						  className={'shadow-rose-400 hover:shadow-md rounded-full'}/>
+				</a>
 			</div>
 		</section>
 	)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 		<body
-			className={`${montserrat.className} antialiased`}
+			className={`${montserrat.className} subpixel-antialiased`}
 		>
 		{children}
 		</body>
