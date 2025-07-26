856e3f28517c0192a5bce6b3f3c1412b8cbe25ba
Emma Lucas <emmalucas256@gmail.com>
2025-07-26 12:26:03 -0500

    Remove spaces from resume filename

--- src/app/homepage/headline.tsx
+++ src/app/homepage/headline.tsx
@@ -1,26 +1,27 @@
 import HeadlineImage from "@/app/homepage/headlineImage";
 
 const Headline = () => {
-	return (
-		<section className={'flex justify-between w-full pt-32 pb-12'} id={'home'}>
-			<div className={'flex flex-col items-start justify-center'}>
-				<span className={'font-medium pl-1'}>Hi, I'm</span>
-				<h1 className={'font-extrabold text-6xl'} id={'my-name'}>
-					Emma Lucas
-				</h1>
-				<h2 className={'tracking-wider font-medium text-lg pl-1'}>
-					Full-stack web developer
-				</h2>
+  return (
+    <section className={'flex justify-between w-full pt-32 pb-12'} id={'home'}>
+      <div className={'flex flex-col items-start justify-center'}>
+        <span className={'font-medium pl-1'}>Hi, I'm</span>
+        <h1 className={'font-extrabold text-6xl'} id={'my-name'}>
+          Emma Lucas
+        </h1>
+        <h2 className={'tracking-wider font-medium text-lg pl-1'}>
+          Full-stack web developer
+        </h2>
 
-				<button className={'mt-8'}>
-					<a href={'/Emma Lucas Resume.pdf'} className={'ctaButton font-bold box-border border border-background/30 py-3 px-4 rounded-2xl shadow-rose-400 ' +
-						'hover:shadow-2xl cursor-pointer'}>Download CV</a>
-				</button>
-			</div>
+        <button className={'mt-8'}>
+          <a href={'/Emma_Lucas_Resume.pdf'}
+             className={'ctaButton font-bold box-border border border-background/30 py-3 px-4 rounded-2xl shadow-rose-400 ' +
+               'hover:shadow-2xl cursor-pointer'}>Download CV</a>
+        </button>
+      </div>
 
-			<HeadlineImage/>
-		</section>
-	)
+      <HeadlineImage/>
+    </section>
+  )
 }
 
 export default Headline;
