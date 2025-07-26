e0167323cb9c64839efbf128f3dfe3bba5dbeae8
Emma Lucas <emmalucas256@gmail.com>
2025-07-26 14:15:54 -0500

    Updated theme color palette, improved dark mode

--- src/app/homepage/headlineImage.tsx
+++ src/app/homepage/headlineImage.tsx
@@ -7,67 +7,68 @@ import {useRef} from "react";
 gsap.registerPlugin(useGSAP)
 
 const HeadlineImage = () => {
-  const blobSvg = useRef<SVGSVGElement>(null);
+	const blobSvg = useRef<SVGSVGElement>(null);
 
-  useGSAP(() => {
-    const durations = [10, 12, 14];
+	useGSAP(() => {
+		const durations = [10, 12, 14];
 
-    // Rotate sub-blob circle elements
-    const blobs = blobSvg.current?.getElementsByClassName('blob') || [];
-    for (let i = 0; i < blobs.length; i++) {
-      const startingRotation = gsap.utils.random(0, 360);
+		// Rotate sub-blob circle elements
+		const blobs = blobSvg.current?.getElementsByClassName('blob') || [];
+		for (let i = 0; i < blobs.length; i++) {
+			const startingRotation = gsap.utils.random(0, 360);
 
-      gsap.fromTo(blobs[i], {
-          rotation: startingRotation,
-          transformOrigin: '40% 50%',
-        },
-        {
-          rotation: startingRotation + 360,
-          transformOrigin: '40% 50%',
-          repeat: -1,
-          ease: 'none',
-          duration: durations[i],
-        }
-      )
-    }
+			gsap.fromTo(blobs[i], {
+					rotation: startingRotation,
+					transformOrigin: '40% 50%',
+				},
+				{
+					rotation: startingRotation + 360,
+					transformOrigin: '40% 50%',
+					repeat: -1,
+					ease: 'none',
+					duration: durations[i],
+				}
+			)
+		}
 
-    // Fade in the entire image blob component
-    gsap.fromTo(blobSvg.current, {
-        opacity: 0,
-        y: -15,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 3,
-        ease: 'elastic.out(1, 0.75)'
-      }
-    )
+		// Fade in the entire image blob component
+		gsap.fromTo(blobSvg.current, {
+				opacity: 0,
+				y: -15,
+			},
+			{
+				opacity: 1,
+				y: 0,
+				duration: 3,
+				ease: 'elastic.out(1, 0.75)'
+			}
+		)
 
-  }, [blobSvg.current]);
+	}, [blobSvg.current]);
 
-  return (
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" id="headlineBlob" ref={blobSvg}>
-      <defs>
-        <filter id="goo">
-          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"/>
-          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo"/>
-          <feBlend in2="goo" in="SourceGraphic" result="mix"/>
-        </filter>
-      </defs>
-      <mask id="mask">
-        <g className="blobs">
-          <circle id="mainBlob" cx="200" cy="200" r="140" transform="rotate(0) translate(0, 0) rotate(0)"/>
-          <circle className="blob" cx="160" cy="150" r="100" transform="rotate(0) translate(0, 0) rotate(0)"/>
-          <circle className="blob" cx="140" cy="250" r="80" transform="rotate(0) translate(0, 0) rotate(0)"/>
-          <circle className="blob" cx="260" cy="130" r="80" transform="rotate(0) translate(0, 0) rotate(0)"/>
-        </g>
-      </mask>
-      <rect x="0" y="0" mask="url(#mask)" fill="var(--color-neutral-500)" width="400" height="400"
-            className={'blobBackground'}/>
-      <image xlinkHref={'/portrait.png'} x="50" y="50" mask="url(#mask)" width="300" height="300"/>
-    </svg>
-  )
+	return (
+		<svg version="1.1" xmlns="http://www.w3.org/2000/svg" id="headlineBlob" ref={blobSvg}>
+			<defs>
+				<filter id="goo">
+					<feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"/>
+					<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+										result="goo"/>
+					<feBlend in2="goo" in="SourceGraphic" result="mix"/>
+				</filter>
+			</defs>
+			<mask id="mask">
+				<g className="blobs">
+					<circle id="mainBlob" cx="200" cy="200" r="140" transform="rotate(0) translate(0, 0) rotate(0)"/>
+					<circle className="blob" cx="160" cy="150" r="100" transform="rotate(0) translate(0, 0) rotate(0)"/>
+					<circle className="blob" cx="140" cy="250" r="80" transform="rotate(0) translate(0, 0) rotate(0)"/>
+					<circle className="blob" cx="260" cy="130" r="80" transform="rotate(0) translate(0, 0) rotate(0)"/>
+				</g>
+			</mask>
+			<rect x="0" y="0" mask="url(#mask)" fill="var(--color-red-300)" width="400" height="400"
+					className={'blobBackground'}/>
+			<image xlinkHref={'/portrait.png'} x="50" y="50" mask="url(#mask)" width="300" height="300"/>
+		</svg>
+	)
 }
 
 export default HeadlineImage;
--- src/app/info/contact.tsx
+++ src/app/info/contact.tsx
@@ -7,13 +7,20 @@ const Contact = () => {
 				<a className={'underline'} href={'mailto:emmalucas256@gmail.com'}>emmalucas256@gmail.com</a>
 			</div>
 			<div className={'flex gap-3 mt-2'}>
+				{/*Linkedin icon link*/}
 				<a href={'https://www.linkedin.com/in/emma-lucas-0241b4155/'} target={'_blank'}>
-					<img src={'linkedin.svg'} alt={'LinkedIn'} width={25} height={25}
-						  className={'shadow-rose-400 hover:shadow-md opacity-70 hover:opacity-100'}/>
+					<svg width={25} height={25} viewBox={'0 0 16 16'}
+						  className={'shadow-rose-400 hover:shadow-md opacity-70 hover:opacity-100 fill-(--foreground)'}>
+						<use href={'/linkedin.svg#icon'}/>
+					</svg>
 				</a>
+
+				{/*GitHub icon link*/}
 				<a href={'https://github.com/emlucas'} target={'_blank'}>
-					<img src={'github.svg'} alt={'Github'} width={25} height={25}
-						  className={'shadow-rose-400 hover:shadow-md rounded-full opacity-70 hover:opacity-100'}/>
+					<svg width={25} height={25} viewBox={'0 0 16 16'}
+						  className={'shadow-rose-400 hover:shadow-md rounded-full opacity-70 hover:opacity-100 fill-(--foreground)'}>
+						<use href={'/github.svg#icon'}/>
+					</svg>
 				</a>
 			</div>
 		</section>
