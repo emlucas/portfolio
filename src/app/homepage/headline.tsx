81fb95af43b87b35affac644355a66751ac66bc3
Emma Lucas <emmalucas256@gmail.com>
2025-07-26 15:29:28 -0500

    Updated mobile portrait scaling

--- src/app/homepage/headline.tsx
+++ src/app/homepage/headline.tsx
@@ -2,7 +2,7 @@ import HeadlineImage from "@/app/homepage/headlineImage";
 
 const Headline = () => {
 	return (
-		<section className={'flex justify-between w-full sm:pt-32 pt-[50vw] relative'} id={'home'}>
+		<section className={'flex justify-between w-full sm:pt-32 pt-[55vw] relative'} id={'home'}>
 			<div className={'flex flex-col items-start justify-center pb-12 z-10 mr-0 sm:mr-96 lg:mr-0'}>
 				<span className={'font-medium pl-1'}>Hi, I'm</span>
 				<h1 className={'font-extrabold text-6xl'} id={'my-name'}>
@@ -20,7 +20,7 @@ const Headline = () => {
 			</div>
 
 			<div
-				className={'imgWrapper absolute top-2 right-[5vh] sm:-right-6 md:-right-12 lg:right-0 w-[60vw] h-[60vw] sm:w-[400px] sm:h-[400px]'}>
+				className={'imgWrapper absolute top-2 right-[10vh] sm:-right-6 md:-right-12 lg:right-0 w-[60vw] h-[60vw] sm:w-[400px] sm:h-[400px]'}>
 				<HeadlineImage/>
 			</div>
 		</section>
