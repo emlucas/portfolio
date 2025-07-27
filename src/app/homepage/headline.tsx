2ad7cf0711f020e79346ba81f1de083e980b8a75
Emma Lucas <33404272+emlucas@users.noreply.github.com>
2025-07-27 17:32:38 -0500

    updated spacing on large screens

--- src/app/homepage/headline.tsx
+++ src/app/homepage/headline.tsx
@@ -9,8 +9,8 @@ const caprasimo = Caprasimo({
 
 const Headline = () => {
 	return (
-		<section className={'flex justify-between w-full sm:pt-32 pt-[55vw] relative'} id={'home'}>
-			<div className={'flex flex-col items-start justify-center pb-12 z-10 mr-0 sm:mr-96 lg:mr-0'}>
+		<section className={'flex justify-between w-full pt-[55vw] sm:pt-32 lg:pt-42 relative'} id={'home'}>
+			<div className={'flex flex-col items-start justify-center pb-12 z-10 mr-0 sm:mr-92 lg:mr-0'}>
 				<span className={'font-medium pl-1'}>Hi, I'm</span>
 				<h1 className={'font-extrabold text-6xl ' + caprasimo.className} id={'my-name'}>
 					Emma Lucas
@@ -27,7 +27,7 @@ const Headline = () => {
 			</div>
 
 			<div
-				className={'imgWrapper absolute top-2 right-[calc(12vw+8px)] sm:-right-6 md:-right-12 lg:right-0 size-[60vw] sm:size-[400px]'}>
+				className={'imgWrapper absolute top-2 lg:top-12 right-[calc(12vw+8px)] sm:-right-6 md:-right-12 lg:right-0 size-[60vw] sm:size-[400px]'}>
 				<HeadlineImage/>
 			</div>
 		</section>
