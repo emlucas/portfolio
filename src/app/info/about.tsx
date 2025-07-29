28dd8220a39fc619a30bfc67c54759cbcb42c4e7
Emma Lucas <33404272+emlucas@users.noreply.github.com>
2025-07-29 10:14:09 -0500

    tweaked About Me blurb

--- src/app/info/about.tsx
+++ src/app/info/about.tsx
@@ -1,10 +1,10 @@
 const About = () => {
 	return (
 		<section className={'text-xl py-8 lg:pt-16 font-light tracking-wide'} id={'about'}>
-			I'm a full-stack software developer with over 4 years of experience creating responsive, modern web apps. I
-			especially enjoy front-end development, where I can focus
-			on building clean, accessible interfaces, and delivering a seamless user experience. Originally from
-			Saint&nbsp;Louis, MO, I am currently relocating to Philadelphia, PA.
+			I'm a full-stack software developer with over 4 years of experience building responsive, modern web apps. I
+			have a particular interest in front-end development, where I focus on creating clean, accessible interfaces and
+			delivering smooth, user-friendly experiences. Originally from St.&nbsp;Louis, MO, I'm currently relocating to
+			Philadelphia, PA.
 		</section>
 	)
 }
